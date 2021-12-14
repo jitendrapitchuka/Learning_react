8168f378e6c8000a2834a0354ea3a6e718642198
jithendra pitchuka <jithendra2900@gmail.com>
2021-12-14 20:04:36 +0530

    darkmode for homepage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 //import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setmode] = useState('light');
+
+  const toggleMode= ()=>
+  {
+    if(mode=== 'light'){
+    setmode('dark');
+  document.body.style.backgroundColor='grey';
+    }
+    else{
+    setmode('light');
+    document.body.style.backgroundColor='white';
+  }
+}
   return (
    <>
    
-<Navbar title="Text conversion"/>
+<Navbar title="Text conversion" mode={mode} toggleMode={toggleMode}/>
 <div className='container my-5' >
-<TextForm heading="Enter the text"/>
+<TextForm heading="Enter the text" mode={mode}/>
 {/* <About/> */}
 </div>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 
+
 export default function TextForm(props) {
 
     const handleUpClick=()=>{
@@ -22,22 +23,23 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color:props.mode=== 'dark' ? 'white': 'black'}}>
             <h1>{props.heading}</h1>
 <div className="mb-3">
   
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode=== 'dark' ? 'light': 'light'}} id="myBox" rows="8"></textarea>
 </div>
 <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
 
+
         </div>
-        <div className="container my-4">
+        <div className="container my-4" style={{color:props.mode=== 'dark' ? 'white': 'black'}}>
             <h1>your text summary</h1>
             <p> {text.split(" ").length-1} words and {text.length} characters   </p>
             <p>{0.08 * text.split(" ").length} minutes read</p>
             <h2>preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0? text:"Enter your text to preview"}</p>
         </div>
         </>
     )
